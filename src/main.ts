a61436673c19c49cc33378d6a51910399d93f9c5
Сергей Кобяков <skobyakov.std@gmail.com>
2021-11-11 20:13:10 +0500

    Added https support

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
+import fs from 'fs';
+import path from 'path';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  let httpsOptions = undefined;
+  if (process.env.NODE_ENV === 'production') {
+    httpsOptions = {
+      key: fs.readFileSync(path.join(__dirname, '..', 'private-key.key'), {
+        encoding: 'utf-8',
+      }),
+      cert: fs.readFileSync(
+        path.join(__dirname, '..', 'public-certificate.crt'),
+        { encoding: 'utf-8' },
+      ),
+    };
+  }
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
